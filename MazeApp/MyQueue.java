dcb8ef4b0937e1f9b338011653d74e5fe7bc9d57
michaelm1453 <31252185+michaelm1453@users.noreply.github.com>
2017-10-17 21:22:29 -0500

    Update MyQueue.java

--- MazeApp/MyQueue.java
+++ MazeApp/MyQueue.java
@@ -1,11 +1,45 @@
 import java.util.ArrayList;
+import java.NoSuchElementException
 
 public class MyQueue<T> implements QueueADT<T>
 {
 	ArrayList<T> myQueue = new ArrayList<>();
+	int x = 0;
 	public void enqueue(T item){
-		myQueue.addLast(item);
+		myQueue.add(item);
+		x++;
 
 	}
+	
+	public T dequeue() throws NoSuchElementException
+	{
+		x--;
+		return elements.remove();
+	}
+	
+	public T front() throws NoSuchElementException
+	{
+		return elements.get(0);
+	}
+	
+	public int size()
+	{
+		return x;
+	}
+	
+	public boolean empty()
+	{
+		if(x != 0)
+			return true;
+		return false;
+	}
+	
+	public void clear()
+	{
+		for(int z = 0; z < x; z++)
+			elements[z] = null;
+	}
+	
+	
 
 }
