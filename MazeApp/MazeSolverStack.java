0c37d7ea1f404efbbdf007338149b8c325e72616
michaelm1453 <31252185+michaelm1453@users.noreply.github.com>
2017-10-22 15:26:18 -0500

    Add files via upload

--- MazeApp/MazeSolverStack.java
+++ MazeApp/MazeSolverStack.java
@@ -3,6 +3,7 @@ package MazeApp;
 public class MazeSolverStack extends MazeSolver
 {
     Maze m = new Maze();
+    MyStack<Square> stack = new MyStack<>();
     public MazeSolverStack()
     {
         super(m);
@@ -11,8 +12,26 @@ public class MazeSolverStack extends MazeSolver
     {
         stack.clear();
     }
+    public boolean isEmpty()
+    {
+        if(stack.size() == 0)
+            return true;
+        return false;
+    }
     public void add(Square sq)
     {
-        
+        stack.push(sq);
+    }
+    public Square next()
+    {
+        return stack.top();
+    }
+    
+    
+    public static void main(String[] args)
+    {
+        MazeSolverStack test = new MazeSolverStack();
+        test.solve();
+        System.out.println(test.path());
     }
 }
